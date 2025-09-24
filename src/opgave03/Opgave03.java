10846545d6be225596c599a0d9487b422e724151
CappiDell <caper.christensen@gmail.com>
2025-09-24 11:49:26 +0200

    In action

--- src/opgave03/Opgave03.java
+++ src/opgave03/Opgave03.java
@@ -3,12 +3,18 @@ package opgave03;
 public class Opgave03 {
     public static void main(String[] args) {
         int[] intArray = {23, 8, 202, -13, 42, 6, 53, 17, 22};
+        System.out.println("Our array of integers");
+        opgave02.Opgave02.printNumbers(intArray);
+        System.out.println("the array reversed is: ");
+        opgave02.Opgave02.printNumbers(reverseArray(intArray));
+
     }
-    public static void reverseArray(int[] ints){
+    public static int[] reverseArray(int[] ints){
         int[] reversedArray = new int[ints.length];
         for (int i = 0; i < ints.length; i++) {
-
+            reversedArray[i] = ints[ints.length-(i+1)];
         }
+        return reversedArray;
     }
 }
 
--- src/opgave04/Opgave04.java
+++ src/opgave04/Opgave04.java
@@ -1,4 +1,36 @@
 package opgave04;
 
 public class Opgave04 {
+    public static void main(String[] args) {
+        int[] intArray1 = {1, 3, 5, 7, 9, 12, 125};
+        int[] intArray2 = {1, 4, 7, 9, 13, 15, 17};
+        System.out.println("Our two arrays of integers");
+        System.out.println("The first one: ");
+        opgave02.Opgave02.printNumbers(intArray1);
+        System.out.println("The second one: ");
+        opgave02.Opgave02.printNumbers(intArray2);
+        System.out.println("The merged array");
+        opgave02.Opgave02.printNumbers(mergedArrays(intArray1,intArray2));
+    }
+    public static int[] mergedArrays(int[] ints1,int[] ints2){
+        int[] intsMerged = new int[ints1.length+ints2.length];
+        int j = 0;
+        int k = 0;
+        for (int i = 0; i < intsMerged.length; i++) {
+            if (j < ints1.length && k < ints2.length) {
+                if (ints1[j] <= ints2[k]){
+
+                    intsMerged[i] = ints1[j];
+                    j++;
+                }
+                else {
+                    intsMerged[i] = ints2[k];
+                    k++;
+                }
+            }
+            else if (j < ints1.length) intsMerged[i] = ints1[j];
+            else intsMerged[i] = ints2[k];
+        }
+        return intsMerged;
+    }
 }
