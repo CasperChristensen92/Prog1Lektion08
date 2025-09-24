d90e1987da4c63ea0f0bce564c83e54d6835017b
CappiDell <caper.christensen@gmail.com>
2025-09-24 11:53:47 +0200

    In action

--- src/opgave05/Opgave05.java
+++ src/opgave05/Opgave05.java
@@ -1,4 +1,8 @@
 package opgave05;
 
 public class Opgave05 {
+    public static void main(String[] args){
+        String[] strings = {"hello","world","arsenal","chicken","Potato","Competetion","Calculate","Programming"};
+    }
+
 }
